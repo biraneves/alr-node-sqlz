64a74d20f2a35f7b8afb66244df7bcb9a3695619
Ubirajara Neves <ubirajara.neves@usp.br>
2022-12-28 10:41:21 -0300

    rota para criar pessoa

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/pessoas', PessoaController.retornaTodasAsPessoas);
 router.get('/pessoas/:id', PessoaController.retornaPessoaPorId);
+router.post('/pessoas', PessoaController.criaPessoa);
 
 module.exports = router;
